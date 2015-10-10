5ddc0ed419226141490da15bd8eec08fa0c3eff3
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2015-10-11 00:58:00 +0200

    added transformer to simplfy life

--- README.md
+++ README.md
@@ -8,3 +8,28 @@ Annotate property with `@observable` (just like in the previous polymer version)
 ## notes
 
 Last version works only with modified `observe` you can find [here](https://github.com/dam0vm3nt/observe/tree/reflectable), until the official one gets ported to reflectable or that branch gets merged.
+
+## using the transformer (optional)
+
+Because `observe` (modified) and `polymer-dart` use different mirror systems to make an object usable by polymer and with autonotify you have to annotate it twice (and make
+it mixin/extend both `JsProxy` AND `Obserable`). For example:
+```dart
+
+class ThatBeautifulModelOfMine extends Observable with JsProxy {
+ @reflectable @observable String field1;
+ @reflectable @observable String field2;
+}
+```
+
+This can be annoying. So `polymer_autonotify` will come with a nice transformer that should be run *before* `observe` transformer that will add `polymer-dart` mixin and annotations for you on object already annotated for `observe`. 
+
+This way previous users of `observe` (that already have their object annotated for it) will have nothing to change to use their code with `polymer-dart` and `polymer-autonotify`.
+
+In the example before one should only write:
+```dart
+
+class TheBeautifulModelOfMine extends Observable {
+ @observable String field1;
+ @observable String field2;
+}
+```
--- lib/transformer.dart
+++ lib/transformer.dart
@@ -1,131 +1,242 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// Code transform for @observable. The core transformation is relatively
+/// straightforward, and essentially like an editor refactoring.
+library draft.polymer.autonotify.transformer;
+
 import 'dart:async';
+
+import 'package:analyzer/analyzer.dart';
+import 'package:analyzer/src/generated/ast.dart';
+import 'package:analyzer/src/generated/error.dart';
+import 'package:analyzer/src/generated/parser.dart';
+import 'package:analyzer/src/generated/scanner.dart';
 import 'package:barback/barback.dart';
-import "package:barback/src/transformer/transform.dart";
-import 'package:smoke/codegen/recorder.dart';
-import 'package:smoke/codegen/generator.dart';
-import 'package:code_transformers/resolver.dart';
-import 'package:code_transformers/src/dart_sdk.dart';
-import "package:analyzer/analyzer.dart";
-import 'package:analyzer/src/generated/element.dart';
-import "package:observe/observe.dart";
+import 'package:code_transformers/messages/build_logger.dart';
+import 'package:source_maps/refactor.dart';
+import 'package:source_span/source_span.dart';
 
-class AutoNotifierTransformer extends Transformer with ResolverTransformer {
-  final BarbackSettings _settings;
-  Transform _transform;
-  AssetId _primaryInputId;
-  String _fileSuffix = '_autonotifier_bootstrap';
-  Resolvers resolvers;
+/// A [Transformer] (fully borrowed from `observe` transformer)
+/// that will add "JsProxy" and "@reflectabe" to object decorated with "Observe" and "@obserable"
+/// to simplfy life of people.
+/// This is intended to be run BEFORE observe transformer.
+///
+class AutonotifyTransformer extends Transformer {
+  final bool releaseMode;
+  final bool injectBuildLogsInOutput;
+  final List<String> _files;
+  AutonotifyTransformer(
+      {List<String> files, bool releaseMode, bool injectBuildLogsInOutput})
+      : _files = files,
+        releaseMode = releaseMode == true,
+        injectBuildLogsInOutput = injectBuildLogsInOutput == null
+            ? releaseMode != true
+            : injectBuildLogsInOutput;
+  AutonotifyTransformer.asPlugin(BarbackSettings settings)
+      : _files = _readFiles(settings.configuration['files']),
+        releaseMode = settings.mode == BarbackMode.RELEASE,
+        injectBuildLogsInOutput = settings.mode != BarbackMode.RELEASE;
 
-  AutoNotifierTransformer.asPlugin(this._settings) {
-    resolvers = new Resolvers(dartSdkDirectory);
-  }
-
-/*
-  classifyPrimary(AssetId id) {
-    if (id.path.endsWith(".dart")) {
-      return "dart";
+  static List<String> _readFiles(value) {
+    if (value == null) return null;
+    var files = [];
+    bool error;
+    if (value is List) {
+      files = value;
+      error = value.any((e) => e is! String);
+    } else if (value is String) {
+      files = [value];
+      error = false;
     } else {
-      return null;
+      error = true;
     }
-  }
-*/
-
-  Future<bool> isPrimary(assetOrId) {
-    if (_settings.mode == BarbackMode.DEBUG) {
-      return new Future.value(false);
-    } else {
-      return super.isPrimary(assetOrId);
-    }
+    if (error) print('Invalid value for "files" in the observe transformer.');
+    return files;
   }
 
-  @override
-  Future applyResolver(Transform transform, Resolver resolver) {
-    _transform = transform;
-    _primaryInputId = _transform.primaryInput.id;
-    return _buildSmokeBootstrap(resolver);
+  // TODO(nweiz): This should just take an AssetId when barback <0.13.0 support
+  // is dropped.
+  Future<bool> isPrimary(idOrAsset) {
+    var id = idOrAsset is AssetId ? idOrAsset : idOrAsset.id;
+    return new Future.value(id.extension == '.dart' &&
+        (_files == null || _files.contains(id.path)));
   }
 
-  /// Builds a Smoke bootstrapper that intializes static Smoke access
-  /// and then calls the actual entry point.
-  Future _buildSmokeBootstrap(Resolver resolver) async {
-    // Initialize the Smoke generator and recorder
-    var generator = new SmokeCodeGenerator();
-    Recorder recorder = new Recorder(generator,
-        (lib) => resolver.getImportUri(lib, from: _primaryInputId).toString());
+  Future apply(Transform transform) {
 
-    // Record each class in the library for our generator
-    resolver.libraries.forEach((LibraryElement lib) {
-      List<ClassElement> classes = lib.units.expand((u) => u.types);
-      classes
-          .where((ClassElement clazz) => (clazz.allSupertypes.any((InterfaceType it) =>
-                  it.name == "Observable" || it.name == "ChangeNotifier")) &&
-              (clazz.mixins.any((InterfaceType it) => it.name == "PolymerAutoNotifySupportBehavior") ||
-                  clazz.fields.any((FieldElement fe) => fe.metadata.any(
-                      (ElementAnnotation ea) =>
-                          ea.element.name == "observable")) ||
-                  clazz.accessors.any((PropertyAccessorElement pa) =>
-                      pa.metadata.any((ElementAnnotation ea) => ea.element.name == "observable"))))
-          .forEach((ClassElement clazz) {
-        //print("${_primaryInputId}: Recording ${clazz.name} from ${lib.name}");
+    return transform.primaryInput.readAsString().then((content) {
+      // Do a quick string check to determine if this is this file even
+      // plausibly might need to be transformed. If not, we can avoid an
+      // expensive parse.
+      if (!observableMatcher.hasMatch(content)) return null;
 
-        recorder.runQuery(
-            clazz,
-            new QueryOptions(
-                includeProperties: true,
-                includeFields: true,
-                includeInherited: false,
-                matches: (String name) => !name.startsWith("_")));
-      });
-    });
-
-    // Generate the Smoke bootstrapper
-    StringBuffer sb = new StringBuffer();
-    sb.write('library polymer_autonotifier.smoke_bootstrap;\n\n');
-    generator.writeImports(sb);
-    sb.write('\n');
-    generator.writeTopLevelDeclarations(sb);
-    sb.write('\ninitSmokeWithStaticConfiguration() => useGeneratedCode(\n');
-    generator.writeStaticConfiguration(sb);
-    // Call the entry point's main method
-    sb.write(');\n');
-
-    //print ("Conf generated:${sb}\n");
-
-    // Add the Smoke bootstrapper to the output files
-    var bootstrapId = _primaryInputId.changeExtension('${_fileSuffix}.dart');
-    _transform.addOutput(new Asset.fromString(bootstrapId, sb.toString()));
-
-    Asset origPrimaryAsset = await _transform.getInput(_primaryInputId);
-    String origAssetContent = await origPrimaryAsset.readAsString();
-    String p = bootstrapId.path;
-    int pi = p.lastIndexOf("/");
-    if (pi >= 0) {
-      p = p.substring(pi + 1);
-    }
-    String result = origAssetContent
-        .replaceAll(
-            new RegExp("import\\s*['\"]package:smoke/mirrors.dart['\"]\s*;"),
-            "import '${p}' as _notif;")
-        .replaceAll(new RegExp("useMirrors\\(\\);"),
-            "_notif.initSmokeWithStaticConfiguration();");
-    //print("MODIFIED ASSET:${result}");
-    _transform.addOutput(new Asset.fromString(_primaryInputId, result));
-  }
-
-  String get allowedExtensions => '.dart';
-/*
-  apply(AggregateTransform transforms) async {
-
-    do {
-      Transform transform = await newTransform(transforms);
-      Resolver resolver = await resolvers.get(transform);
-      try {
-        await applyResolver(transform,resolver);
-      } finally {
-        resolver.release();
+      var id = transform.primaryInput.id;
+      // TODO(sigmund): improve how we compute this url
+      var url = id.path.startsWith('lib/')
+          ? 'package:${id.package}/${id.path.substring(4)}'
+          : id.path;
+      var sourceFile = new SourceFile(content, url: url);
+      var logger = new BuildLogger(transform,
+          convertErrorsToWarnings: !releaseMode,
+          detailsUri: 'http://goo.gl/5HPeuP');
+      var transaction = _transformCompilationUnit(content, sourceFile, logger);
+      if (!transaction.hasEdits) {
+        transform.addOutput(transform.primaryInput);
+      } else {
+        var printer = transaction.commit();
+        // TODO(sigmund): emit source maps when barback supports it (see
+        // dartbug.com/12340)
+        printer.build(url);
+        transform.addOutput(new Asset.fromString(id, printer.text));
       }
-    } while(true);
+
+      if (injectBuildLogsInOutput) return logger.writeOutput();
+    });
+  }
+}
+
+TextEditTransaction _transformCompilationUnit(
+    String inputCode, SourceFile sourceFile, BuildLogger logger) {
+  var unit = parseCompilationUnit(inputCode, suppressErrors: true);
+  var code = new TextEditTransaction(inputCode, sourceFile);
+  for (var directive in unit.directives) {
+    if (directive is LibraryDirective && _hasObservable(directive)) {
+      logger.warning(NO_OBSERVABLE_ON_LIBRARY,
+          span: _getSpan(sourceFile, directive));
+      break;
+    }
   }
 
-*/
+  for (var declaration in unit.declarations) {
+    if (declaration is ClassDeclaration) {
+      _transformClass(declaration, code, sourceFile, logger);
+    } else if (declaration is TopLevelVariableDeclaration) {
+      if (_hasObservable(declaration)) {
+        logger.warning(NO_OBSERVABLE_ON_TOP_LEVEL,
+            span: _getSpan(sourceFile, declaration));
+      }
+    }
+  }
+  return code;
 }
+
+_getSpan(SourceFile file, AstNode node) => file.span(node.offset, node.end);
+
+/// True if the node has the `@observable` or `@published` annotation.
+// TODO(jmesserly): it is not good to be hard coding Polymer support here.
+bool _hasObservable(AnnotatedNode node) =>
+    node.metadata.any(_isObservableAnnotation) && node.metadata.every(_isNotPropertyAnnotation);
+
+// TODO(jmesserly): this isn't correct if the annotation has been imported
+// with a prefix, or cases like that. We should technically be resolving, but
+// that is expensive in analyzer, so it isn't feasible yet.
+bool _isObservableAnnotation(Annotation node) =>
+    _isAnnotationContant(node, 'observable') ||
+        _isAnnotationType(node, 'ObservableProperty');
+
+bool _isNotPropertyAnnotation(Annotation node) =>
+  !_isAnnotationContant(node,"property") &&
+  !_isAnnotationContant(node,"refletable") &&
+  !_isAnnotationType(node,"Property") &&
+  !_isAnnotationType(node,"PolymerReflectable");
+
+bool _isAnnotationContant(Annotation m, String name) =>
+    m.name.name == name && m.constructorName == null && m.arguments == null;
+
+bool _isAnnotationType(Annotation m, String name) => m.name.name == name;
+
+void _transformClass(ClassDeclaration cls, TextEditTransaction code,
+    SourceFile file, BuildLogger logger) {
+  if (_hasObservable(cls)) {
+    logger.warning(NO_OBSERVABLE_ON_CLASS, span: _getSpan(file, cls));
+  }
+
+  // We'd like to track whether observable was declared explicitly, otherwise
+  // report a warning later below. Because we don't have type analysis (only
+  // syntactic understanding of the code), we only report warnings that are
+  // known to be true.
+  if (cls.extendsClause != null) {
+    var id = _getSimpleIdentifier(cls.extendsClause.superclass.name);
+    if (id.name == 'Observable') {
+      if (cls.withClause==null) {
+        code.edit(id.offset, id.end, 'JsProxy with Observable');
+      } else {
+        code.edit(cls.withClause.mixinTypes[0].offset,cls.withClause.mixinTypes[0].offset,"JsProxy,");
+      }
+    }
+  }
+
+  if (cls.withClause != null) {
+    for (var type in cls.withClause.mixinTypes) {
+      var id = _getSimpleIdentifier(type.name);
+      if (id.name == 'Observable') {
+        if (!_getSimpleIdentifier(cls.extendsClause.superclass.name)=='PolymerElement') {
+          code.edit(id.offset, id.end, 'Observable,JsProxy');
+        }
+      } else if (id.name == 'ChangeNotifier') {
+        break;
+      }
+    }
+  }
+
+
+  // Track fields that were transformed.
+  var instanceFields = new Set<String>();
+
+  for (var member in cls.members) {
+    if (member is FieldDeclaration) {
+      if (member.isStatic) {
+        continue;
+      }
+      if (_hasObservable(member)) {
+        _transformFields(file, member, code, logger);
+
+      }
+    }
+  }
+
+}
+
+SimpleIdentifier _getSimpleIdentifier(Identifier id) =>
+    id is PrefixedIdentifier ? id.identifier : id;
+
+bool _hasKeyword(Token token, Keyword keyword) =>
+    token is KeywordToken && token.keyword == keyword;
+
+String _getOriginalCode(TextEditTransaction code, AstNode node) =>
+    code.original.substring(node.offset, node.end);
+
+bool _isReadOnly(VariableDeclarationList fields) {
+  return _hasKeyword(fields.keyword, Keyword.CONST) ||
+      _hasKeyword(fields.keyword, Keyword.FINAL);
+}
+
+void _transformFields(SourceFile file, FieldDeclaration member,
+    TextEditTransaction code, BuildLogger logger) {
+  final fields = member.fields;
+
+  String metadata = '';
+  if (fields.variables.length > 0) {
+    code.edit(member.metadata.first.offset,member.metadata.first.offset,"@reflectable ");
+
+  }
+
+}
+
+Token _findFieldSeperator(Token token) {
+  while (token != null) {
+    if (token.type == TokenType.COMMA || token.type == TokenType.SEMICOLON) {
+      break;
+    }
+    token = token.next;
+  }
+  return token;
+}
+
+// TODO(sigmund): remove hard coded Polymer support (@published). The proper way
+// to do this would be to switch to use the analyzer to resolve whether
+// annotations are subtypes of ObservableProperty.
+final observableMatcher =
+    new RegExp("@(observable|Observable)");
